1c038dfee313c1de166273436d04cf6c4fcd8c70
Alexey Pavlovskiy <pavlovskiy.alexey@gmail.com>
2021-01-22 17:15:58 +0200

    first commit

---
+++ Assets/Scrips/Game/UI/MainMenu.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scrips.Common.Actions;
+using Assets.Scrips.UI;
+using Assets.Scripts.Common.UI.Controller;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Assets.Scrips.Game.UI
+{
+    public class MainMenu: WindowController
+    {
+	    public class ButtonsContainer: SlotsContainer<MenuButton> {}
+
+	    [SerializeField]
+	    private MenuButton _buttonPref;
+
+	    [SerializeField]
+	    private Transform _container;
+
+	    private ButtonsContainer _buttons = new ButtonsContainer();
+
+	    protected override void OnInit()
+	    {
+		    _buttons.Initialize(_buttonPref, _container);
+
+			var showAction = new ParallelAction();
+			var showActions = new SequenceAction();
+			showAction.Add(showActions);
+
+			var hideAction = new ParallelAction();
+			var hideActions = new SequenceAction();
+			hideAction.Add(hideActions);
+
+		    for (int i = 0; i < 4; i++)
+		    {
+			    var btn = _buttons.AddSlot();
+			    if (btn == null)
+			    {
+				    return;
+			    }
+
+			    btn.Initialize(i, OnButtonClick);
+
+			    showActions.Add(new ExecuteAction(() =>
+			    {
+					btn.Open();
+			    }));
+			    showActions.Add(new WaitAction(0.3f));
+
+			    hideActions.Add(new ExecuteAction(() =>
+			    {
+				    btn.Close();
+			    }));
+			    hideActions.Add(new WaitAction(0.3f));
+		    }
+
+		    showAction.Add(new CallbackAction(finish =>
+		    {
+			    foreach (var btn in _buttons.Slots)
+			    {
+				    if (btn.GetState() != MenuButton.State.Open)
+						return;
+			    }
+
+			    finish();
+		    }));
+
+		    showAction.Add(new CallbackAction(finish =>
+		    {
+			    foreach (var btn in _buttons.Slots)
+			    {
+				    if (btn.GetState() != MenuButton.State.Close)
+					    return;
+			    }
+
+			    finish();
+		    }));
+
+		    ShowAction = showAction;
+		    HideAction = hideAction;
+	    }
+
+	    private void CreateOpenAction()
+	    {
+
+	    }
+
+	    private void OnButtonClick(int obj)
+	    {
+		    Close();
+	    }
+
+	    private void OnDestroy()
+	    {
+	    }
+    }
+}
---
+++ Assets/Scrips/Game/UI/MenuButton.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.UI;
+
+namespace Assets.Scrips.Game.UI
+{
+	public class MenuButton: MonoBehaviour
+	{
+		public enum State
+		{
+			None,
+			Close,
+			Open
+		}
+
+		[SerializeField]
+		private Button _button;
+
+		[SerializeField]
+		private Animator _animator;
+
+		[SerializeField]
+		private string _closeStateName = "Close";
+
+		[SerializeField]
+		private string _openStateName = "Open";
+
+		[SerializeField]
+		private string _stateName = "IsShow";
+
+		private Action<int> _callback;
+
+		public int Index { get; private set; }
+
+		private void Awake()
+		{
+			_button.onClick.AddListener(() =>
+			{
+				_callback.Invoke(Index);
+			});
+		}
+
+		public void Initialize(int index, Action<int> callback)
+		{
+			Index = index;
+			_callback = callback;
+		}
+
+		public void Open()
+		{
+			if (_animator == null)
+				return;
+
+			_animator.SetBool(_stateName, true);
+		}
+
+		public void Close()
+		{
+			if (_animator == null)
+				return;
+
+			_animator.SetBool(_stateName, false);
+		}
+
+		public State GetState()
+		{
+			if (_animator == null)
+			{
+				return State.None;
+			}
+
+			var transitionInfo = _animator.GetAnimatorTransitionInfo(0);
+			if (transitionInfo.IsUserName(_closeStateName))
+			{
+				return State.Close;
+			}
+			if (transitionInfo.IsUserName(_openStateName))
+			{
+				return State.Open;
+			}
+
+			return State.None;
+		}
+	}
+}
